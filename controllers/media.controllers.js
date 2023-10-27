8524b1f6e57364c7a934428f78116420aa59c4b0
“nameisjar” <nthia4@gmail.com>
2023-10-27 13:10:28 +0700

    memperbaiki bug

--- controllers/media.controllers.js
+++ controllers/media.controllers.js
@@ -65,7 +65,7 @@ module.exports = {
                 status: true,
                 message: 'OK',
                 error: null,
-                data: { file_url: userProfile } // cek data dari request
+                data: { file_url: url, first_name, last_name, birth_date }
             });
         } catch (err) {
             next(err);
